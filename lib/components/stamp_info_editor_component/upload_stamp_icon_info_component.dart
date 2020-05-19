7630ea4f77de5053c74c73c4c2895a1edb29c4c3
Siratee Kittiwitchaowakul <maiyarapkung@Siratees-iMac.local>
2020-05-19 14:25:12 +0700

    Feature: add image picker

--- lib/components/stamp_info_editor_component/upload_stamp_icon_info_component.dart
+++ lib/components/stamp_info_editor_component/upload_stamp_icon_info_component.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:psm_at_stamp/components/signin_button_components.dart';
 import 'package:psm_at_stamp/services/psmatstamp_users_services/PsmAtStampUser_constructure.dart';
 
@@ -16,13 +19,24 @@ class UploadStampIconInfoComponent extends StatefulWidget {
 
 class _UploadStampIconInfoComponentState
     extends State<UploadStampIconInfoComponent> {
+  File _image;
+  Future<void> pickImage() async {
+    var image = await ImagePicker.pickImage(source: ImageSource.gallery);
+    setState(() {
+      _image = image;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
+        _image == null ? Container() : Image.file(_image),
         signInButtonComponent(
-          title: "Test",
-          onPressHandler: () {},
+          title: "อัพโหลดรูป Logo ฐานกิจกรรม",
+          onPressHandler: () {
+            pickImage();
+          },
         ),
       ],
     );
